8f6765390821a3302c647ef893b66d23e7f4d5a3
Dhivya Aroma <74200707+dhivyaaroma@users.noreply.github.com>
2024-03-06 11:13:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,23 @@ provider "google" {
   zone    = "asia-south1-a"
 }
 
-resource "google_compute_network" "vpc_network" {
+/*resource "google_compute_network" "vpc_network" {
   name = "terraform-demo01"
+}*/
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }

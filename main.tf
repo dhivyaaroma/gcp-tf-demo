86aa5da726ec52b61e45e529f36a362a77393c66
Dhivya Aroma <74200707+dhivyaaroma@users.noreply.github.com>
2024-03-06 10:51:07 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:/Users/dhivy/learn-terraform-gcp/da-apigee-tf-281f997fe4bd.json")
+
+  project = "da-apigee-tf"
+  region  = "asia-south1"
+  zone    = "asia-south1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
